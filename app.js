deed684fe37e51a4d5c78fe4f4a6f2975ebf01ee
Aditya Saxena <aaddii091@gmail.com>
2024-05-14 01:11:01 +0530

    removing config file

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
+app.use("/api/v1/users", userRoutes);
+
+app.all();
 module.exports = app;

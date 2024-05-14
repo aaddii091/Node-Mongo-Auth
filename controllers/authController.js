c3993eeb3aaae628ab3213c3a15524f994fd4584
Aditya Saxena <aaddii091@gmail.com>
2024-05-15 03:49:32 +0530

    Password Validators;

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,26 +3,19 @@ const User = require('./../models/userModel');
 exports.login = catchAsync((req, res, next) => {});
 exports.signUp = catchAsync(async (req, res, next) => {
   console.log(req.body);
-  try {
-    const newUser = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
 
-    // Send a success response
-    res.status(200).json({
-      status: 'success',
-      data: {
-        user: newUser, // Include the newly created user in the response
-      },
-    });
-  } catch (error) {
-    // Handle any errors that occur during user creation
-    console.error('Error creating user:', error);
-    res.status(500).json({
-      status: 'error',
-      message: 'Error creating user',
-    });
-  }
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // Send a success response
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: newUser, // Include the newly created user in the response
+    },
+  });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -9,11 +10,25 @@ const userSchema = new mongoose.Schema({
     type: String,
     lowercase: true,
     required: [true, 'Please provide an email'],
+    unique: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
 });
 
